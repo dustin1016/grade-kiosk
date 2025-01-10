c49babf2e9afb58f05c687999dd2ae0895f2a7a1
dustin1016 <106299264+dustin1016@users.noreply.github.com>
2025-01-10 09:50:25 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     setStudentData(null)
     setIsFetching(true)
     try {
-      const response = await fetch("http://10.125.2.222:8080/rptapi/student.php/x", {
+      const response = await fetch("https://10.125.2.222/student.php/x", {
         method: "POST", // Use POST to send data in the body
         headers: {
           "Content-Type": "application/json", // Tell the server you're sending JSON
@@ -24,13 +24,13 @@ function App() {
         body: JSON.stringify({ id:query }), // Send the query in the request body
       });
 
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.error || "Unknown error occurred");
-      }
+      // if (!response.ok) {
+      //   const errorData = await response.json();
+      //   throw new Error(errorData.error || "Unknown error occurred");
+      // }
 
       const data = await response.json();
-    
+      console.log(data)
       setStudentData(data.studentDetails[0]);
     } catch (error) {
       setErrorMsg(error.message)
